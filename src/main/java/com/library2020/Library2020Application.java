dc8b4ec57a5f6662b2f52b9f4eb39ff4685c1cae
Vasylenko Olexandr <31988254+VassalX@users.noreply.github.com>
2020-02-06 11:28:58 +0200

    Initial commit

---
+++ src/main/java/com/library2020/Library2020Application.java
@@ -0,0 +1,13 @@
+package com.library2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Library2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Library2020Application.class, args);
+	}
+
+}
